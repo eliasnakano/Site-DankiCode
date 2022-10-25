f16a13d6d9c1d665150010e52dec287cf61c8f16
eliasnakano <eliasmoral1237@outlook.com>
2022-10-25 14:46:36 -0300

    primeiro commit

---
+++ index.php
@@ -0,0 +1,178 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Productrunt.com.br</title>
+    <link rel="stylesheet" type="text/css" href="styles.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="slick/slick.css" />
+    <link rel="stylesheet" type="text/css" href="slick/slick-theme.css" />
+    <link rel="icon" type="image/png" sizes="32x32" href="img/favicon.ico">
+</head>
+<section class="topo">
+    <div class="center">
+        <header>
+            <div class="logo">
+                <img src="./img/logo.png">
+            </div>
+            <!---logo--->
+        </header>
+        <ul class="menu-desktop">
+            <li><a href="#"> Inicio </a></li>
+            <li><a href="#"> Sobre </a></li>
+            <li><a href="#"> Contato </a></li>
+        </ul>
+        <div class="clear"></div>
+        </br>
+        </br>
+        <div class="w50 descricao">
+            <h2>Melhore a comunicação com seu cliente e time.</h2>
+            <p>Consultoria especializada em startups,empresas,principalmente pessoas.</p>
+            <a target="_blank" href="#">Ver Demostração</a>
+        </div>
+        <!--w50_-->
+        <div class="w50 time-imagem">
+            <img src="./img/equipe.png">
+        </div>
+        <!--w50-->
+        <div class="clear"></div>
+    </div>
+    <!---center--->
+</section>
+<!---topo--->
+<!-- Slider de cases -->
+<section class="slider">
+    <div class="carousel">
+        <div><img class="prim-img" src="img/amazon.jpg" alt="drone">
+        </div>
+        <div><img class="seg-img" src="img/dominos.jpg" alt="keybord">
+        </div>
+        <div><img class="ter-img" src="img/uber.jpg" alt="keybord">
+        </div>
+        <div>
+            <img class="qui-img" src="img/walmart.jpg" alt="keybord">
+        </div>
+        <div><img class="qua-img" src="img/costco.jpg" alt="keybord">
+        </div>
+        <div><img class="prim-img" src="img/amazon.jpg" alt="drone">
+        </div>
+        <div><img class="seg-img" src="img/dominos.jpg" alt="keybord">
+        </div>
+        <div><img class="ter-img" src="img/uber.jpg" alt="keybord">
+        </div>
+        <div>
+            <img class="qui-img" src="img/walmart.jpg" alt="keybord">
+        </div>
+        <div><img class="qua-img" src="img/costco.jpg" alt="keybord">
+        </div>
+    </div>
+    <!--carrousel-->
+</section>
+<hr>
+<section class="diferenciais">
+    <div class="center">
+        <h2>Contribuímos de ponta a ponta</h2>
+        <div class="icons-diferenciais">
+            <div class="box-single-diferenciais">
+                <img src="img/icon1.png" />
+                <h3>Ambientes Mobile</h3>
+                <p>Garanta que toda sua comunicação esteja alinhada com seu propósito, cada palavra conta.</p>
+            </div>
+            <!--box-single-diferenciais-->
+            <div class="box-single-diferenciais">
+                <img src="img/icon2.png" />
+                <h3>Ambientes Mobile</h3>
+                <p>Garanta que toda sua comunicação esteja alinhada com seu propósito, cada palavra conta.</p>
+            </div>
+            <!--box-single-diferenciais-->
+            <div class="box-single-diferenciais">
+                <img src="img/icon3.png" />
+                <h3>Ambientes Mobile</h3>
+                <p>Garanta que toda sua comunicação esteja alinhada com seu propósito, cada palavra conta.</p>
+            </div>
+            <!--box-single-diferenciais-->
+        </div>
+        <!--icons-diferenciais-->
+    </div>
+</section>
+<section class="time">
+    <div class="center">
+        <div class="w50 time-descricao">
+            <h2>Um Time experiente,comunicador e coesco.</h2>
+            <p>Trabalhar em equipe é um desafio que traz muitas recompensas. Mentes diferentes conectadas para alcançar o mesmo objeto, possuem mais chances de vitória. Para criar um ambiente de trabalho coeso e com muita harmonia, </p>
+            <p>selecionamos as melhores frases de equipe.
+             Confira e leve a motivação necessária para o seu time continuar batendo as metas e ter ótimos resultados!</p>
+        </div>
+        <!--w50 time-->
+        <div class="w50 img-time">
+            <img src="./img/time.png">
+        </div>
+        <div class="clear"></div>
+    </div>
+    <!--center-->
+</section>
+<!--section-time-->
+<section class="depoimentos">
+    <h1>Depoimentos</h1>
+    <p>A citação é um dos primeiros tipos de depoimentos (e um dos mais fáceis de se conseguir). Surgindo primeiramente nos jornais, atualmente as citações são comuns em páginas de produtos.
+    </p>
+</section>
+<!--depoimentos section-->
+<footer>
+    <div class="center">
+        <ul>
+            <i>&nbsp;Email:productrunt@hotmail.com &nbsp;</i>
+            <i>&nbsp;Telefone:9999-9999</i>
+        </ul>
+
+        <div class="header-social-media">
+            <ul>
+                <li>
+                    <a href="#">
+                        <i class="fab fa-facebook-f">&nbsp;</i>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <i class="fab fa-twitter">&nbsp; </i>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <i class="fab fa-instagram">&nbsp; </i>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <i class="fab fa-github">&nbsp; </i>
+                    </a>
+                </li>
+        </div>
+        <div class="center">
+            <div class="logo2">
+                <img src="./img/logo-footer.png">
+            </div>
+        </div>
+    </div> <!--center-->
+</footer>
+<script src="https://code.jquery.com/jquery-2.2.0.min.js" type="text/javascript"></script>
+<script src="slick/slick.js"></script>
+<script>
+    $('.carousel').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+
+    })
+</script>
+
+</body>
+
+</html>
